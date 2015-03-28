ea01c95dbc2bedcd5d12c0e6d92fa8f632de2ef9
Wing-Hou Chan <winghouchan@gmail.com>
2015-03-28 20:49:58 +0000

    Commit files

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,58 +1,62 @@
 'use strict';
 
-$.get('http://api.ipify.org?format=text&callback=?',
-  function(text) {
-    $('input[name="user_ip"]').val(text);
-  }
-);
+$(function() {
 
-$('.close-trigger').click(function() {
-  $('aside.open').removeClass('open');
-  return false;
-});
-
-$('form#proposal').submit(function(event) {
-  event.preventDefault();
-
-  // var check_login = function() {
-
-  // };
-
-  var form_valid = function() {
-    if($('[required]').val().length === 0) {
-      return false;
-    } else {
-      return true;
+  $.get('http://api.ipify.org?format=text&callback=?',
+    function(text) {
+      $('input[name="user_ip"]').val(text);
     }
-  };
+  );
 
-  console.log(form_valid)
+  $('.close-trigger').click(function() {
+    $('aside.open').removeClass('open');
+    return false;
+  });
 
-  var success = function() {
-    console.log('Success');
-  };
+  $('form#proposal').submit(function(event) {
+    event.preventDefault();
 
-  var error = function() {
-    console.log('Error');
-  }
-  
-  if(form_valid) {
+    // var check_login = function() {
+
+    // };
+
+    var form_valid = function() {
+      if($('[required]').val().length === 0) {
+        return false;
+      } else {
+        return true;
+      }
+    };
+
+    console.log(form_valid)
+
+    var success = function() {
+      console.log('Success');
+    };
+
+    var error = function() {
+      console.log('Error');
+    }
     
-    $.ajax({
-      method: 'POST',
-      url: 'http://codeluca.me/create',
-      data: {
-        'ip': $('input[name="user_ip"]').val(),
-        'name': $('input[name="project_name"]').val(),
-        'desc': $('textarea[name="project_desc"]').val(),
-        'photo': null,
-        'lat': Lat,
-        'lng': Lng
-      },
-      success: success,
-      error: error
-    });
-  } else {
-    console.log('Requirements failed');
-  }
+    if(form_valid) {
+      
+      $.ajax({
+        method: 'POST',
+        url: 'http://codeluca.me/create',
+        data: {
+          'ip': $('input[name="user_ip"]').val(),
+          'name': $('input[name="project_name"]').val(),
+          'desc': $('textarea[name="project_desc"]').val(),
+          'photo': null,
+          'lat': Lat,
+          'lng': Lng
+        },
+        success: success,
+        error: error
+      });
+    } else {
+      console.log('Requirements failed');
+    }
+  });
+
 });
